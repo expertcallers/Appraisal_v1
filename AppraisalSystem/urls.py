f1d864ca09c0df317ceb55a05812f8bc70ddf516
Ibrahim Khan <ibrahim7353khan@gmail.com>
2022-03-30 18:07:23 +0530

    Updated main urls

--- AppraisalSystem/urls.py
+++ AppraisalSystem/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from appraisalapp.views import *
 
 urlpatterns = [
     path('appraisal/admin/', admin.site.urls),
+    path('accounts/login/', IndexPage),
     path('appraisal/',include('appraisalapp.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
