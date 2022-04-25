bcce2134f0d8870f41df705b9c796d6a79f17e8a
Ibrahim Khan <ibrahim7353khan@gmail.com>
2022-04-25 12:39:03 +0530

    Added Import Export option in admin.py

--- appraisalapp/admin.py
+++ appraisalapp/admin.py
@@ -1,25 +1,38 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
+class ProfileResource(resources.ModelResource):
+  class Meta:
+     model = Profile
+     # fields = ['emp_name', 'emp_id', 'emp_desi', 'emp_process', "emp_rm1", "emp_rm2", "emp_rm3"]
 
-class ProfileSearch(admin.ModelAdmin):
+class ProfileSearch(ImportExportModelAdmin):
     search_fields = ('emp_name', 'emp_id', "emp_desi", 'emp_process')
     list_display = ('emp_name', 'emp_id', 'emp_desi', 'emp_process', "emp_rm1", "emp_rm2", "emp_rm3")
+    resource_class = ProfileResource
 
 
-class DataSearch(admin.ModelAdmin):
+class DataSearchResource(resources.ModelResource):
+  class Meta:
+     model = Data
+
+class DataSearch(ImportExportModelAdmin):
     search_fields = ('emp_name', 'emp_id', "emp_desi", 'emp_process')
     list_display = ('emp_name', 'emp_id', 'emp_desi', 'emp_process', "emp_rm1", "emp_rm2", "emp_rm3")
+    resource_class = DataSearchResource
 
 
-class TableSearch(admin.ModelAdmin):
+class TableSearch(ImportExportModelAdmin):
     search_fields = ('emp_name', 'emp_id')
     list_display = ('emp_name', 'emp_id', "agent_score", "mgr_score")
 
 
-class PartDSearch(admin.ModelAdmin):
+class PartDSearch(ImportExportModelAdmin):
     search_fields = ('emp_name', 'emp_id')
     list_display = ('emp_name', 'emp_id')
+    resource_class = ProfileResource
 
 
 # Register your models here.
